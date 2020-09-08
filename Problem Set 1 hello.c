b19b597f971ffba282a3cc5eab55dcc032cffab4
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-08 20:58:04 +0700

    CS50X 2020 Problem Set 1 hello.c  Implementation

--- Problem Set 1 hello.c
+++ Problem Set 1 hello.c
@@ -1,1 +1,23 @@
+/**
+    [PROGRAM] :  Hello world!
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
 
+    CS50x 2020 Problem Set 1 hello.c
+
+    Link to the  Problem Set
+    https://cs50.harvard.edu/x/2020/psets/1/hello/#:~:text=pset1
+*/
+
+#include <stdio.h>
+#include <cs50.h> /* include cs50.h */
+
+int main(int argc, char *argv[]) /* the river Code */
+{
+    //user input name
+    string name = get_string("What is your name?\n");
+    printf("hello  %s\n", name);
+
+    return 0;// signal to operating system everything works fine
+
+}/** End of main function */
