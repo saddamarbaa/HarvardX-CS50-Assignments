5673f53b5e61ad06fd1a7ce168a7ff1d2fb1e224
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-09-11 01:37:55 +0700

    CS50X Problem Set 2 Substitution (Substitution Cipher)

--- pset 2 Substitution Cipher.c
+++ pset 2 Substitution Cipher.c
@@ -1,108 +1,109 @@
-
 /**
-* CS 50  2020
-* Problem Set 2
-* substitution.c
-* substitution cipher, “encrypt”  a message
-* by replacing every letter with another letter from the 26 charter key
-* key should  contain alphabetic chartaters only  and also must not contain repeated  chartaters
-*/
+    [PROGRAM] :  Substitution(Substitution Cipher)
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
+
+    CS50X 2020 Problem Set 2 Substitution Cipher Implementation
+    
+    substitution cipher is a program that “encrypt” a message by
+    replacing every letter with another letter from the 26 charterers key.
+    key should contain alphabetic charterers only and also must not
+    contain repeated  charterers .
+    
+    Link to the Week 2 lecture
+    https://youtu.be/8PrOp9t0PyQ
+
+    Link to the  Problem Set
+    https://cs50.harvard.edu/x/2020/psets/2/substitution/#:~:text=substitution%20cipher */
+
 #include <stdio.h>
-#include <cs50.h>
-#include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <cs50.h>    /* include cs50.h  Header file */
+#include <string.h>  /* include string.h Header file */
+#include <ctype.h>  /* include ctype.h Header file */
 
-const int UPERCALE_INDEX = 65; //Upper case letter 'A' ASCII index
-const int LOWERCALE_INDEX = 97;  //lower case letter 'a' ASCII index
-const int NUMBER_OF_ALPHATE_INDEX = 26;   // number of English alphabet letters
+/**  Global constant variable declarations */
 
-int main(int argc, string argv[])
+const int UPER_CASE_INDEX = 65;     /* Upper case letter 'A' ASCII index */
+
+const int LOWER_CASE_INDEX = 97;    /* lower case letter 'a' ASCII index */
+
+const int NUMBER_OF_ALPHABET_INDEX = 26;  /* number of English alphabet letters */
+
+int main(int argc, string argv[]) /* the river Code */
 {
-    // key from user throw command line
-    string UeserKey = argv[1];
+    // get key from user throw command line arguments
+    string user_Key = argv[1];
 
     // checking if user enter only single arguments
     if (argc != 2)
     {
         printf("Usage: ./substitution key \n");
-        return 1;
+        return 1; // signal to operating system that program not successes
     }
-    int keylength = strlen(UeserKey);   // key length
+    int key_Length = strlen(user_Key);  //  the length of given
 
     //checking if length of the key is 26 digits
-
-    if (keylength != NUMBER_OF_ALPHATE_INDEX)
+    if (key_Length != NUMBER_OF_ALPHABET_INDEX)
     {
         printf("Key must contain 26 characters.\n");
-        return 1;
+        return 1; // signal to operating system that program not successes
     }
 
-    // check for non_alphabtical character in the key
+    // check for non alphabetical character in the key
     // as the key  must only contain alphabetic characters
-
-    for (int i = 0 ; i < keylength ; i++)
+    for (int i = 0; i < key_Length; i++)
     {
-        if (!isalpha(UeserKey[i]))
+        if (!isalpha(user_Key[i])) // call isalpha() function
         {
             printf("key must only contain alphabetic characters \n");
-
-            return 1;
+            return 1; // signal to operating system that program not successes
         }
     }
-    //  check for repeated  characters
 
-    for (int i = 0 ; i < keylength; i++)
+    // check for repeated  characters
+    for (int i = 0; i < key_Length; i++)
     {
-        for (int j = i + 1 ; j < keylength; j++)
+        for (int j = i + 1; j < key_Length; j++)
         {
-            if (UeserKey[i] == UeserKey[j])
+            if (user_Key[i] == user_Key[j])
             {
                 printf("key must not contain repeated  characters . \n");
-                return 1;
+                return 1; // signal to operating system that program not successes
             }
         }
     }
 
-
+    // get the plaintext from user
     string plaintext = get_string("plaintext: "); // ueser plaintext
+
+    /** encrypting process start from here */
 
     printf("ciphertext: ");
 
+    /* Iterate throw user plaintext */
     for (int i = 0, n = strlen(plaintext); i < n; i++)
     {
         // if char is lowercase
-        if (islower(plaintext[i]))
+        if (islower(plaintext[i]))  //call islower() function
         {
-            int inedxlow = plaintext[i] - 97;  //substuted with char in the same i inedx from key
-            printf("%c", tolower(UeserKey[inedxlow]));
+            int inedxlow = plaintext[i] - LOWER_CASE_INDEX;  // Substituted with char in the same i index from key
+            printf("%c", tolower(user_Key[inedxlow]));
         }
-
         // if char is uppercase
         else if (isupper(plaintext[i]))
         {
-            int  indxup = plaintext[i] - 65;
-            printf("%c", toupper(UeserKey[indxup])); //substuted with char in the same i inedx from key
+            int  indxup = plaintext[i] - UPER_CASE_INDEX;
+            printf("%c", toupper(user_Key[indxup])); //Substituted with char in the same i index from key
         }
-        else
+        else // else cases
         {
-            //print as  is it without chane
+            //print as is it without change
             printf("%c", plaintext[i]);
         }
     }
-
     printf("\n");
 
-    return 0;
-}
+    return 0;// signal to operating system everything works fine
 
-
-
-
-
-
-
-
-
-
-
-
+} /** End of main function */
