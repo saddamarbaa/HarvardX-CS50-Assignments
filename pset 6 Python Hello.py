49f45050f742d65af7fa71021d268019d789ba6d
saddamarbaa <51326421+saddamarbaa@users.noreply.github.com>
2020-10-03 15:06:49 +0700

    CS50X Problem Set 6 Hello World (Python)

--- pset 6 Python Hello.py
+++ pset 6 Python Hello.py
@@ -1,2 +1,19 @@
+"""
+    [PROGRAM] :  Hello world!
+    [AUTHOR]  :  Saddam Arbaa
+    [Email]   :  <saddamarbaas@gmail.com>
+    CS50x 2020 Problem Set 6 hello.py
+    A program that says hello to the world
 
- 
+    Link to the Week 6 lecture
+    CS50 2019 - Lecture 6 - Python
+    https://youtu.be/fL308_-Kbt0 """
+
+# define main function
+def main():
+
+    name = input("What is your name?")  # Prompt user for name
+    print(f"hello,{name}")           # say Hello to user
+
+# call main
+main()
